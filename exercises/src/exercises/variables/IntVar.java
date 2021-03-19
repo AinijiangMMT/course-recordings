b9c00a1ae2c14ba2715302e7ef5e4301596241f2
abdugheni <ainijiang@outlook.com>
2021-03-19 13:24:40 +0100

    initial commit

---
+++ exercises/src/exercises/variables/IntVar.java
@@ -0,0 +1,15 @@
+package exercises.variables;
+
+public class IntVar {
+	public static void main(String[] args) {
+		int n1, n2, n3 = 5;
+		n1 = 76;
+		n2 = 34;
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		
+
+	}
+
+}
